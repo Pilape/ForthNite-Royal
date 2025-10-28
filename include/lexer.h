2ddfdcdef20f08821c09a225d12a90f49944db85
pilape <leon.l.somme@gmail.com>
2025-10-28 11:07:14 +0100

    Lexer boilerplate

--- include/lexer.h
+++ include/lexer.h
@@ -1,4 +1,19 @@
 #ifndef LEXER_HEADER
 #define LEXER_HEADER
 
+#include <stdlib.h>
+
+#define LEXEME_MAX_LENGTH 32
+
+typedef struct {
+    char lexeme[LEXEME_MAX_LENGTH-1];
+
+} Token;
+
+typedef struct {
+    size_t length;
+    size_t capacity;
+    Token* data;
+} TokenList;
+
 #endif 
