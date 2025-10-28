9e8d670853bfbfe6679ae1ffeac0712bb35dd3da
pilape <leon.l.somme@gmail.com>
2025-10-28 10:51:19 +0100

    Added file loading

---
+++ include/compiler.h
@@ -0,0 +1,13 @@
+#ifndef COMPILER_HEADER
+#define COMPILER_HEADER
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ASSERT(condition, msg, ...)                    \
+    if (!(condition)) {                                \
+        fprintf(stderr, "[ERROR]: " msg "\n", __VA_ARGS__); \
+        exit(-1);                                      \
+    }                                                  \
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "./../include/compiler.h"
+
+size_t GetFileLength(char* path) {
+    FILE* fp = fopen(path, "r");
+
+    ASSERT(fp != NULL, "Failed to open file: '%s'", path);
+
+    fseek(fp, 0, SEEK_END);
+    size_t file_length = ftell(fp) + 1; // For the "\0"
+    rewind(fp);
+
+    fclose(fp);
+
+    return file_length;
+}
+
+void GetFileData(char* path, char* dest, size_t file_length) {
+    FILE* fp = fopen(path, "r");
+
+    ASSERT(fp != NULL, "Failed to open file: '%s'", path);
+
+    fread(dest, file_length, 1, fp);
+
+    fclose(fp);
+}
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
@@ -7,9 +32,11 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
+    size_t file_length = GetFileLength(argv[1]);
+    char source[file_length];
+    GetFileData(argv[1], source, file_length);
 
-
-    printf("Hello world!\n");
+    printf("%s\n", source);
 
     return 0;
 }
